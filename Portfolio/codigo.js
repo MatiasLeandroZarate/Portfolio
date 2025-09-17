32a41c1325bedbb672b3c7666008c593c7a10a59
Matias Zarate <m.zarate92@hotmail.com>
2025-09-17 15:53:08 -0300

    Section Conocimiento

--- Portfolio/codigo.js
+++ Portfolio/codigo.js
@@ -1,1 +1,17 @@
-function hola(){}
+const expecienciaBtn = document.querySelectorAll('.experiencia-btn');
+
+expecienciaBtn.forEach((btn, idx) => {
+    btn.addEventListener('click', () => {
+        const experienciaDetail = document.querySelectorAll('.experiencia-detail');
+        
+        expecienciaBtn.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        experienciaDetail.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        experienciaDetail[idx].classList.add('active');
+    });
+});
