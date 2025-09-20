0a60629bd7abbc4880aa76d9f1ccb8f12013ddf7
Matias Zarate <m.zarate92@hotmail.com>
2025-09-19 21:17:26 -0300

    Contact terminado

--- Portfolio/codigo.js
+++ Portfolio/codigo.js
@@ -1,3 +1,52 @@
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+
+const PaginaActiva = () => {
+    const header = document.querySelector('header'); 
+
+    header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    }, 110);
+
+    navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    
+}
+  
+navLinks.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if (!link.classList.contains('active')) {
+            PaginaActiva();
+            
+            link.classList.add('active');
+            
+            setTimeout(() => {
+                sections[idx].classList.add('active');
+            },110);
+        }
+    });
+});
+
+logoLink.addEventListener('click', () => {
+    if (!navLinks[0].classList.contains('active')) {
+        PaginaActiva();
+
+        navLinks[0].classList.add('active');
+
+        setTimeout(() => {
+            sections[0].classList.add('active');
+        },110);
+    }
+});
+
+
 const expecienciaBtn = document.querySelectorAll('.experiencia-btn');
 
 expecienciaBtn.forEach((btn, idx) => {
